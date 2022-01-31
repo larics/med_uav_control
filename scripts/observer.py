ab4bf4bb03070e193b09c2b01defcb895bced93e
Filip Zoric <fzoric8@gmail.com>
2022-01-31 17:32:15 +0100

    Added observer for timing duration of flight and recording of rosbags in launch

--- scripts/observer.py
+++ scripts/observer.py
@@ -4,27 +4,33 @@ import roslib
 import rospy
 import rosbag
 import datetime
+import copy
 from std_msgs.msg import Float32
 from sensor_msgs.msg import Joy
 from nav_msgs.msg import Odometry
 
 # https://www.javatpoint.com/how-to-create-a-dataframes-in-python
+# Rosbag cookbok http://wiki.ros.org/rosbag/Cookbook
+# https://answers.ros.org/question/52773/record-with-rosbag-from-launch-file/
 
 # TODO: 
 # - Add exporting to CSV
 # - Add timer and watching time 
-# - 
+# - Add sucessful recording of full bag file 
+
 class Observer: 
 
     def __init__(self): 
 
         rospy.init_node("observer", log_level=rospy.INFO)
 
+        # Publishers
         self.duration_pub = rospy.Publisher("/duration", Float32, queue_size=1)
 
-        rospy.Subscriber("/bebop/odometry", Odometry, self.OdometryCallback)
-        rospy.Subscriber("/joy", Joy, self.JoyCallback)
-        rospy.Subscriber("/duration", Float32, self.DurationCallback)
+        # Subscribers
+        rospy.Subscriber("/bebop/odometry", Odometry, self.OdometryCallback, queue_size=1)
+        rospy.Subscriber("/joy", Joy, self.JoyCallback, queue_size=1)
+        rospy.Subscriber("/duration", Float32, self.DurationCallback, queue_size=1)
 
         self.current_pos_x = 0; self.current_pos_y = 0; 
         # Define goal pose
@@ -36,11 +42,15 @@ class Observer:
         self.start_time = rospy.Time.now().to_sec(); now = datetime.datetime.now(); 
         # Current date
         date = "{}-{}-{}-{}-{}".format(now.day, now.month, now.year, now.hour, now.minute)
+
+        sleep_time = 10
+        rospy.sleep(sleep_time)
         # Bag name
         self.bag = rosbag.Bag('/home/developer/catkin_ws/src/med_uav_control/experiments/{}.bag'.format(date), 'w')
         # Current rate
-        self.rate = rospy.Rate(10)
+        self.rate = rospy.Rate(5)
 
+        self.odom_recv = False; self.joy_recv = False
         self.initialized = True
 
         self.duration_pub.publish(Float32(0.0))
@@ -54,15 +64,25 @@ class Observer:
     
     def JoyCallback(self, msg):
 
-        self.bag.write("/joy", msg)
+        self.joy_recv = True
+
+        self.joy_msg = Joy()
+        self.joy_msg.header = msg.header
+        self.joy_msg.axes = msg.axes
+        self.joy_msg.buttons = msg.buttons
 
 
     def OdometryCallback(self, msg): 
 
+        self.odom_recv = True
+
         self.current_pos_x = msg.pose.pose.position.x
         self.current_pos_y = msg.pose.pose.position.y
 
-        self.bag.write('/bebop/odometry', msg)
+        self.odom_msg = Odometry()
+        self.odom_msg.header = msg.header
+        self.odom_msg.pose = msg.pose
+        self.odom_msg.twist = msg.twist
 
     def in_range(self, pos, upper_limit, lower_limit): 
  
@@ -80,7 +100,6 @@ class Observer:
             rospy.sleep(0.1)
             
         else:
-
         
             while True: 
                 
@@ -99,8 +118,13 @@ class Observer:
                         timeout = True
                     else:
                         timeout = False
-
-                    rospy.loginfo("...")
+                    
+                    # Write odometry message and joy message to bag 
+                    if self.odom_recv and self.joy_recv:
+                        odom_msg = copy.deepcopy(self.odom_msg); joy_msg = copy.deepcopy(self.joy_msg)
+                        self.bag.write("/bebop/odometry", odom_msg)
+                        rospy.loginfo("...")
+                        self.bag.write("/joy", joy_msg)
 
                 else:
 
@@ -117,22 +141,15 @@ class Observer:
 
                         self.duration_pub.publish(Float32(time_elapsed))
 
-                    
                     break
 
+                self.rate.sleep()
 
-            self.rate.sleep()
+
             self.bag.close()
-            exit()
-
-
-
-
-
-                    
-                    
-
-
+                
+            exit()              
+                  
 
 if __name__ == "__main__": 
 
