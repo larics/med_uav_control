f68dbe805df14d7b7ccf64008c478889905efe85
Filip Zoric <fzoric8@gmail.com>
2022-06-29 13:58:30 +0200

    Added minor updates to enable HPE control

--- scripts/bebop_joy_override.py
+++ scripts/bebop_joy_override.py
@@ -10,7 +10,7 @@ class BebopJoyOverride:
 
     def __init__(self):
         # Publisher to ardrone cmd_vel topic, can be run in namespace
-        self.cmdVelPub = rospy.Publisher("cmd_vel_real", Twist, queue_size=1)
+        self.cmdVelPub = rospy.Publisher("cmd_vel", Twist, queue_size=1)
         self.takeoffPub = rospy.Publisher("takeoff", Empty, queue_size=1)
         self.landPub = rospy.Publisher("land", Empty, queue_size=1)
         self.resetPub = rospy.Publisher("reset", Empty, queue_size=1)
@@ -19,16 +19,22 @@ class BebopJoyOverride:
         self.joyData = Joy()
         self.bebopCmdVelReal = Twist() # Publishing to real cmd_vel
         self.bebopCmdVel = Twist() # Subscribing to user cmd_vel
+        self.hpeCmdVel = Twist()    
         self.overrideControl = 0
+        self.hpeOverrideControl = 0
         
         # Load joy parameters
         self.takeoff_index = rospy.get_param("~bebop_joy/takeoff_index")
         self.land_index = rospy.get_param("~bebop_joy/land_index")
         self.override_index = rospy.get_param("~bebop_joy/override_index")
+        self.hpe_override_index = rospy.get_param("~bebop_joy/hpe_override_index")
 
         # Subscriber to joystick topic
         rospy.Subscriber("/joy", Joy, self.JoyCallback, queue_size=1)
         rospy.Subscriber("cmd_vel", Twist, self.CmdVelCallback, queue_size=1)
+
+        # Subscriber to hpe cmd_vel topic
+        rospy.Subscriber("hpe_cmd_vel", Twist, self.HpeCmdVelCallback, queue_size=1)
 
     def run(self):
         r = rospy.Rate(50)
@@ -40,20 +46,24 @@ class BebopJoyOverride:
             else:
                 rospy.loginfo_throttle(5.0, "[BebopJoyOverride] override ON")
                 self.cmdVelPub.publish(self.bebopCmdVelReal)
+
+            if self.hpeOverrideControl == 1.0: 
+                rospy.loginfo_throttle(1.0, "[BebopJoyOverride] override HPE-ON")
+
             r.sleep()
 
     def JoyCallback(self, data):
         # Assign data to joy variable
         self.joyData = data
 
-        #print self.joyData
+        # Scale fact for slower control 
+        scale_fact = 0.25
 
         # Setting joy values to be command values for bebop
-        self.bebopCmdVelReal.linear.x = self.joyData.axes[3]
-        self.bebopCmdVelReal.linear.y = self.joyData.axes[2]
-        self.bebopCmdVelReal.linear.z = self.joyData.axes[1]
-        self.bebopCmdVelReal.angular.z = self.joyData.axes[0]
-        #self.cmdVelPub.publish(self.bebopCmdVelReal)
+        self.bebopCmdVelReal.linear.x = self.joyData.axes[3] * scale_fact
+        self.bebopCmdVelReal.linear.y = self.joyData.axes[2] * scale_fact
+        self.bebopCmdVelReal.linear.z = self.joyData.axes[1] * scale_fact
+        self.bebopCmdVelReal.angular.z = self.joyData.axes[0] * scale_fact
 
         if not self.overrideControl == 0 and self.joyData.buttons[self.takeoff_index] == 1:
             rospy.loginfo("[BebopJoyOverride] Takeoff")
@@ -67,10 +77,24 @@ class BebopJoyOverride:
             rospy.loginfo("[BebopJoyOverride] Reset")
             self.resetPub.publish(Empty())
 
+        # If HPE override button press, send command from HPE
+        if self.hpeOverrideControl == 1: 
+            rospy.loginfo("[BebopJoyOverride] Hpe control active!")
+            self.cmdVelPub.publish(self.hpeCmdVel)
+        # If not, send command from joy
+        else:
+            rospy.loginfo("[BebopJoyOverride] Joy control active!")
+            self.cmdVelPub.publish(self.bebopCmdVel)
+
+        # Override flags
         self.overrideControl = self.joyData.buttons[self.override_index]
+        self.hpeOverrideControl = self.joyData.buttons[self.hpe_override_index]
 
     def CmdVelCallback(self, msg):
         self.bebopCmdVel = msg
+
+    def HpeCmdVelCallback(self, msg): 
+        self.hpeCmdVel = msg
 
 if __name__ == "__main__":
     rospy.init_node("BebopJoyOverrideNode")
